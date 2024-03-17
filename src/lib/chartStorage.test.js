c178c6627fd19fb6ae7112525760631cdcef248e
OPLeMaster <owenplemaster@gmail.com>
2024-03-17 11:54:29 -0700

    finished tests for chartStorage

--- src/lib/chartStorage.test.js
+++ src/lib/chartStorage.test.js
@@ -7,24 +7,23 @@ const {saveChart, loadAllSavedCharts, loadSavedChart, updateCurrentChartData,loa
 require('@testing-library/jest-dom');
 
 
+//Set up for mock of localStorage from Jamie on Stack Overflow
+//Jamie. (2022, Dec. 11). "How do I deal with localStorage in jest tests?"
+//URL: https://stackoverflow.com/questions/32911630/how-do-i-deal-with-localstorage-in-jest-tests/41434763#41434763
+
 //mock local storage
 const setItemMock = jest.fn();
 const getItemMock = jest.fn();
 
 beforeEach(() => {
-    Object.defineProperty(window, 'localStorage', {
-        value: {
-            getItem: getItemMock,
-            setItem: setItemMock
-        },
-        writable: true
-    });
+    Storage.prototype.setItem = setItemMock;
+    Storage.prototype.getItem = getItemMock;
 });
 
 afterEach(() => {
-    jest.clearAllMocks();
+    setItemMock.mockRestore();
+    getItemMock.mockRestore();
 });
-
 
 describe('testing the functions in chartStorage with valid input', () => {
 
@@ -79,3 +78,96 @@ describe('testing the functions in chartStorage with valid input', () => {
     });
 });
 
+describe('testing the loadAllSavedCharts function', () => {
+
+    test('returns an empty array if there are currently no saved graphs', () => {
+        //Arrange
+        getItemMock.mockReturnValueOnce(JSON.stringify([]));
+
+        //Act
+        const result = loadAllSavedCharts();
+
+        //Assert
+        expect(result).toEqual([]);
+    });
+
+    test('returns the list of all charts stored', () => {
+        //Arrange
+        const charts = [{title: '1'}, {title: '2'}];
+        getItemMock.mockReturnValueOnce(JSON.stringify(charts));
+
+        //Act
+        const result = loadAllSavedCharts();
+
+        //Assert
+        expect(result).toEqual(charts);
+    });
+});
+
+describe('testing the loadSavedChart function', () => {
+
+    test('returns an empty object if the given index doesnt exist', () => {
+        //Arrange
+        const charts = [{title: '1'}, {title: '2'}];
+        getItemMock.mockReturnValueOnce(JSON.stringify(charts));
+
+        //Act
+        const result = loadSavedChart(3);
+
+        //Assert
+        expect(result).toEqual({});
+    });
+
+    test('returns the chart given by the index', () => {
+        //Arrange
+        const charts = [{title: '1'}, {title: '2'}];
+        getItemMock.mockReturnValueOnce(JSON.stringify(charts));
+
+        //Act
+        const result = loadSavedChart(1);
+
+        //Assert
+        expect(result).toEqual({title: '2'});
+    });
+});
+
+describe('testing the updateCurrentChartData function', () => {
+
+    test('saves the current chart being worked on in local storage', () => { 
+        //Arrange
+        const chart = {title: 'work in progress'};
+
+        //Act
+        updateCurrentChartData(chart);
+
+        //Assert
+        expect(setItemMock).toHaveBeenCalledTimes(1);
+        expect(setItemMock).toHaveBeenNthCalledWith(1, 'currentChartData', JSON.stringify(chart));
+    });
+});
+
+describe('testing the loadCurrentChartData function', () => {
+
+    test('returns an empty object if there is no cuurent chart saved in local storage', () => {
+        //Arrange
+        getItemMock.mockReturnValueOnce(JSON.stringify({}));
+
+        //Act
+        const result = loadCurrentChartData();
+
+        //Assert
+        expect(result).toEqual({});
+    });
+
+    test('returns the current chart being worked on from locale storage', () => {
+        //Arrange
+        const chart = {title: 'work in progress'};
+        getItemMock.mockReturnValueOnce(JSON.stringify(chart));
+
+        //Act
+        const result = loadCurrentChartData();
+
+        //Assert
+        expect(result).toEqual(chart);
+    });
+});
